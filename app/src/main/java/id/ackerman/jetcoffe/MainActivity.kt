1a593f066d02bb669432c7dbe0b542e5502fcd42
Harun AR-Rashid <harun4arrashid@gmail.com>
2023-06-06 22:52:41 +0700

    Banner

--- app/src/main/java/id/ackerman/jetcoffe/MainActivity.kt
+++ app/src/main/java/id/ackerman/jetcoffe/MainActivity.kt
@@ -3,14 +3,22 @@ package id.ackerman.jetcoffe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import id.ackerman.jetcoffe.ui.components.SearchBar
 import id.ackerman.jetcoffe.ui.theme.JetCoffeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,6 +34,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
+
+    Column {
+        Banner()
+    }
+
+}
+
+@Composable
+fun Banner(modifier: Modifier = Modifier) {
+
+    Box(modifier = modifier) {
+
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop, // Fit, Fill Bounds
+            modifier = Modifier.height(160.dp)
+        )
+
+        SearchBar()
+
+    }
 
 }
 
--- app/src/main/java/id/ackerman/jetcoffe/ui/components/SearchBar.kt
+++ app/src/main/java/id/ackerman/jetcoffe/ui/components/SearchBar.kt
@@ -1,11 +1,15 @@
 package id.ackerman.jetcoffe.ui.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -15,22 +19,31 @@ import id.ackerman.jetcoffe.ui.theme.JetCoffeTheme
 
 @Composable
 fun SearchBar(modifier: Modifier = Modifier) {
-
     TextField(
         value = "",
         onValueChange = {},
-        leadingIcon = {
-
+        leadingIcon = { // slot yg dpt di isi dgn Composable Function lain
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
         },
-        color = TextFieldDefaults.textFieldColors(
-            backgroundColor = MaterialTheme.colors.surface,
-            disabledIndicatorColor = Color.Transparent,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedLabelColor = Color.Transparent
-        ),
-        modifier = modifier.padding(16.dp)
-    )
 
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface,
+            disabledIndicatorColor = Color.Transparent, // indicator biar garis bawah nya ilang
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+
+        placeholder = { Text(stringResource(R.string.placeholder_search)) }, // hint
+
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .heightIn(min = 48.dp) // height min from text
+            .clip(RoundedCornerShape(16.dp))
+    )
 }
 
 @Preview(showBackground = true)
